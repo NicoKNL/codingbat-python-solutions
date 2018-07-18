9952bf28435e9ca747afc52b32c0fab98168c2c0
NicoK <klaassen.nico@gmail.com>
2018-07-18 20:30:25 +0200

    Added list-1

---
+++ list-1/max_end3.py
@@ -0,0 +1,3 @@
+def max_end3(nums):
+  max_n = max(nums[::2])
+  return [max_n, max_n, max_n]
---
+++ list-1/sum2.py
@@ -0,0 +1,7 @@
+def sum2(nums):
+  if not nums:
+    return 0
+  if len(nums) == 1:
+    return nums[0]
+  else:
+    return sum(nums[:2])
